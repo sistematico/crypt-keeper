b4f9ef3f8efc425f1541f48eac5598809e0c0c34
Lucas Salies Brum <lucas@archlinux.com.br>
2021-09-07 12:12:30 -0400

    Update automático

--- packetbot/cogs/reply.py
+++ packetbot/cogs/reply.py
@@ -2,27 +2,27 @@ from discord.ext import commands
 import discord
 import random
 
-cli = discord.Client()
+client = discord.Client()
 
-class Reply():
-    def __init__(self, bot, config, cli):
-        self.cli = cli
+class Reply(client):
+    def __init__(self, bot, config, client):
+        self.client = client
         self.bot = bot
         self.config = config[__name__.split(".")[-1]]
         self.saudacoes = ["Olá", "Oi"]
 
-    @cli.event
+    @client.event
     async def on_message(message):
-        if message.author == cli.user:
+        if message.author == client.user:
             return
         
         if message.content == 'oi':
             await message.send("Oiii")
             #await message.channel.send(' aaaaa ')
-        await cli.process_commands(message)
+        await client.process_commands(message)
 
-    @commands.command()
-    async def tip(self, ctx):
-        """Get a random tip about using the bot."""
-        index = random.randrange(len(self.tips))
-        await ctx.send(f"**Dica #{index+1}:** {self.tips[index]}")
+    # @commands.command()
+    # async def tip(self, ctx):
+    #     """Get a random tip about using the bot."""
+    #     index = random.randrange(len(self.tips))
+    #     await ctx.send(f"**Dica #{index+1}:** {self.tips[index]}")
