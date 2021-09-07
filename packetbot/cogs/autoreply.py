ae935b53062c0c76bdccb819357ee0887cc0f0b3
Lucas Salies Brum <lucas@archlinux.com.br>
2021-09-07 12:59:47 -0400

    Update automático

--- packetbot/cogs/autoreply.py
+++ packetbot/cogs/autoreply.py
@@ -4,7 +4,8 @@ import discord
 class AutoReply(commands.Cog):
     def __init__(self, bot, config):
         self.bot = bot
-        self.config = config[__name__.split(".")[-1]]
+        self.config = config
+        #self.config = config[__name__.split(".")[-1]]
 
     @commands.Cog.listener()
     async def on_message(self, message):
