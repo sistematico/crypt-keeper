308d4c2435dea0639848510a0f94297d37949418
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-09-28 14:00:46 -0400

    Atualizações

--- packetbot/cogs/reply.py
+++ packetbot/cogs/reply.py
@@ -22,15 +22,16 @@ class Reply(commands.Cog):
         if message.content.startswith('!'):
             return
 
-        if any(x in message.content.lower() for x in ['frase do dia']):
-            await ctx.send(f"hello, {ctx.author.name}")
+        if any(x in message.content.lower() for x in ['frase do dia']) or if message.content.startswith('@frase'):
+            await message.channel.send("{message.author.id} {message.author.name}".format(message))
             frase_do_dia = random.choice(list(open('txt/confucio.txt','r')))
-            await message.channel.send("{0.author.name} {frase_do_dia}".format(message))
+            #await message.channel.send("{0.author.name} {frase_do_dia}".format(message))
+            #await message.channel.send("{0.author.name} {frase_do_dia}".format(message))
             return
 
         if any(x in message.content.lower() for x in ['oie', 'olá','e aí']):
             saudacao = random.choice(['Olá!', 'Oi, tudo bom?', 'E aí! Blza?'])
-            await message.channel.send("{0.author} {saudacao}".format(message))
+            await message.channel.send("{message.author} {saudacao}".format(message))
             return
 
         # if 'olá' in message.content.lower():
--- packetbot/cogs/utils.py
+++ packetbot/cogs/utils.py
@@ -9,10 +9,13 @@ class Util(commands.Cog):
         if message.author == self.bot.user:
             return
 
-        if message.content == '@clear all' and message.author.permissions_in(message.channel).manage_messages:
-            deleted = await message.channel.purge(limit=100, check=lambda msg: not msg.pinned)
-            await message.channel.send('100 mensagens foram apagadas.'.format(deleted))
-        elif not message.author.permissions_in(message.channel).manage_messages:
-            await message.channel.send("{0.author} você precisa de permissões para apagar as mensagens em massa.".format(message))
+        if message.content == '@clear all':
+            if message.author.permissions_in(message.channel).manage_messages:
+                deleted = await message.channel.purge(limit=100, check=lambda msg: not msg.pinned)
+                await message.channel.send('100 mensagens foram apagadas.'.format(deleted))
+            elif not message.author.permissions_in(message.channel).manage_messages:
+                await message.channel.send("{0.author} você precisa de permissões para apagar as mensagens em massa.".format(message))
+            
+            return
 
         await self.bot.process_commands(message)
