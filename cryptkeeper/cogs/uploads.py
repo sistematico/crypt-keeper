b210a941a7556d1b5c2a36f56f7ff88aef97d462
Lucas Salies Brum <lucas@archlinux.com.br>
2021-10-02 18:25:06 -0400

    Update

--- cryptkeeper/cogs/uploads.py
+++ cryptkeeper/cogs/uploads.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
 
-
 class Upload(commands.Cog):
     def __init__(self, bot, config):
         self.bot = bot
@@ -11,20 +10,10 @@ class Upload(commands.Cog):
             return
 
         if len(message.attachments) > 0:
-            #filename = ''.join(e for e in ctx.message.attachments[0].filename if e.isalnum())
             filename = message.attachments[0].filename.replace('/(?:\.(?![^.]+$)|[^\w.])+/g', '-').lower()
-
-            if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.gif'):
-                await message.attachments[0].save('uploads/imagens/' + filename)
-                await message.channel.send('Arquivo: `' + filename + '` enviado com sucesso.')
             if filename.endswith('.mp3'):
                 await message.attachments[0].save('uploads/audio/' + filename)
                 await message.channel.send('Arquivo: `' + filename + '` enviado com sucesso.')
-            else:
-                await message.channel.send('Formato inválido.')
-
-            #await message.channel.send(filename)
-            # await clear_soundboard()
         return
 
-        await self.bot.process_commands(message)
+        #await self.bot.process_commands(message)
